1e06585cc9adfa12e7cc58dc9edc4e6476553cf8
José Luis Landabaso Díaz <landabaso@gmail.com>
2023-05-12 20:19:15 +0200

    esplora: More robust error handling in queries. Also, a queue mechanism to avoid rate limit

--- src/esplora.ts
+++ src/esplora.ts
@@ -4,34 +4,84 @@ import { ESPLORA_BLOCKSTREAM_URL } from './constants';
 
 import type { Explorer } from './interface';
 
+//https://github.com/Blockstream/esplora/issues/449#issuecomment-1546000515
+class RequestQueue {
+  private queue: (() => void)[] = [];
+  private delayBetweenRequests: number;
+  private lastRequestTime: number = 0;
+
+  constructor(delayBetweenRequests: number) {
+    this.delayBetweenRequests = delayBetweenRequests;
+  }
+
+  async fetch(...args: Parameters<typeof fetch>): Promise<Response> {
+    return new Promise((resolve, reject) => {
+      const fetchTask = async () => {
+        const now = Date.now();
+        const timeSinceLastRequest = now - this.lastRequestTime;
+        if (timeSinceLastRequest < this.delayBetweenRequests) {
+          // If the delay has not passed yet, wait for the remaining time
+          await new Promise(resolve =>
+            setTimeout(
+              resolve,
+              this.delayBetweenRequests - timeSinceLastRequest
+            )
+          );
+        }
+        try {
+          const response = await fetch(...args);
+          this.lastRequestTime = Date.now();
+          resolve(response);
+        } catch (error) {
+          reject(error);
+        }
+      };
+      this.queue.push(fetchTask);
+      this.processQueue();
+    });
+  }
+
+  private processQueue() {
+    if (this.queue.length > 0) {
+      const fetchTask = this.queue.shift();
+      if (fetchTask) fetchTask();
+    }
+  }
+}
+//250 milliseconds between consecutive calls.
+//Blockstream ends up cutting the replies at 200ms consecutive calls
+const requestQueue = new RequestQueue(210);
+
+async function esploraFetch(
+  ...args: Parameters<typeof fetch>
+): Promise<Response> {
+  const response = await requestQueue.fetch(...args);
+  if (!response.ok) {
+    const errorDetails = await response.text();
+    throw new Error(
+      `Network request failed! Status code: ${response.status} (${response.statusText}). URL: ${response.url}. Server response: ${errorDetails}`
+    );
+  }
+  return response;
+}
+
 async function esploraFetchJson(
   ...args: Parameters<typeof fetch>
 ): Promise<unknown> {
-  const response = await fetch(...args);
-  if (response.status !== 200) {
-    throw new Error('Service is down!');
-  }
+  const response = await esploraFetch(...args);
   try {
     const json = await response.json();
     return json;
   } catch (error) {
-    throw new Error('Invalid json format!');
+    throw new Error('Failed to parse server response as JSON!');
   }
 }
 
 async function esploraFetchText(
   ...args: Parameters<typeof fetch>
 ): Promise<string> {
-  const response = await fetch(...args);
-  if (response.status !== 200) {
-    throw new Error('Service is down!');
-  }
-  try {
-    const text = await response.text();
-    return text;
-  } catch (error) {
-    throw new Error('Invalid text format!');
-  }
+  const response = await esploraFetch(...args);
+  return await response.text();
 }
 
 function isValidHttpUrl(string: string): boolean {
